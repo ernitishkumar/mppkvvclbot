5f744b48e1def938cd2bfce1074eb595312b9790
Nitish Kumar <erkumarnitish@gmail.com>
2016-12-22 17:37:22 +0530

    First build

---
+++ src/main/java/com/mppkvvclbot/MppkvvclbotApplication.java
@@ -0,0 +1,12 @@
+package com.mppkvvclbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MppkvvclbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MppkvvclbotApplication.class, args);
+	}
+}
